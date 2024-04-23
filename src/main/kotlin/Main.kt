e1ba325ba1ef6c63ac4052b8b5659c122a09f84f
Ppajingae <111533728+Ppajingae@users.noreply.github.com>
2024-04-23 21:38:16 +0900

    While문에 True값을 넣고 처리

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,74 +9,118 @@ fun main() {
     println("입력하고 싶은 값과 연산자를 띄워쓰기 단위로 입력해주세요 ex > 10 + 5")
 
     //BufferedReader 가 코테 할때 조금더 빠른 걸 보여 줘서 쓰긴 썻는데 왜 빠른지 잘 모르 겠습니다
+    //while에 true를 넣고 중간에 break로 반복문 탈출 처리
     val br = BufferedReader(InputStreamReader(System.`in`))
-    try {
-        var tempBr = br.readLine().split(" ")
-        var num1 = tempBr[0].toInt()
-        var num2 = tempBr[2].toInt()
-
-        var operator = tempBr[1]
-        var i: Int = 0
-        var tempNum:Int = 0
+    
+    //몇 번 연산 했는지 체크
+    var i: Int = 0
+    
+    //연산을 여러번 했을 경우 저장할 변수
+    var tempNum: Int = 0
 
 
-        while(operator != "="){
+    while (true) {
+        try {
+            //수 연산자 수 순으로 띄워쓰기 기준으로 입력
+            val tempBr = br.readLine().split(" ")
+            //1번째 연산 이후에 새로 담을 변수 지정
             var newNum: Int
+            
+            if (i == 0) {
+                var num1 = tempBr[0].toInt()
+                var num2 = tempBr[2].toInt()
+                
+                //연산자 검사 로직
+                var operatorString: String = tempBr[1]
+                var operator:Char = ' '
+                
+                //연산자를 String으로 받았으므로 최초 연산자만 뽑아오도록 설계
+                for(j in 0 until operatorString.length) {
+                    if(operatorString[j] == '+' || operatorString[j] == '-' || operatorString[j] == '*' || operatorString[j] == '/' || operatorString[j] == '%') {
+                        operator = operatorString[j]
+                    }
+                }
+                //위에 로직에서 연산자가 처리되지 않을 경우 연산자를 입력 받도록 설계
+                while (operator == ' ') {
+                    println("연산자를 다시 입력해주세요, 연산자는 최초 입력된 연산자로 계산이 됩니다")
+                    var operatorResult = br.readLine().toString()
+                    for (k in 0 until operatorResult.length) {
+                        if (operatorResult[k] == '+' || operatorResult[k] == '-' || operatorResult[k] == '*' || operatorResult[k] == '/' || operatorResult[k] == '%') {
+                            operator = operatorResult[k]
+                        }
+                    }
+                }
 
-            if(i == 0) {
+                
+                
+                //임시 저장공간에 계산 값을 저장
                 tempNum += operatorFunction(true, num1, operator, num2, tempNum)
-            }else{
-                val temp = br.readLine().split(" ")
-                if(temp.size >= 3){
-                    println("겂울 다시 입력해주세요 (2번째 입력 시에는 (기호 숫자) 방식으로 입력 부탁드립니다)")
-                    continue
+
+            } else {
+                var operatorString: String = tempBr[0]
+                var operator:Char = ' '
+                for(j in 0 until operatorString.length) {
+                    if(operatorString[j] == '+' || operatorString[j] == '-' || operatorString[j] == '*' || operatorString[j] == '/' || operatorString[j] == '%' || operatorString[j] == '=') {
+                        operator = operatorString[j]
+                    }
                 }
-                operator = temp[0]
-                if(operator.length > 1){
-                    println("연산자를 다시 입력해주세요")
-                    continue
+                while (operator == ' ') {
+                    println("연산자를 다시 입력해주세요, 연산자는 최초 입력된 연산자로 계산이 됩니다")
+                    var operatorResult = br.readLine().toString()
+                    for (k in 0 until operatorResult.length) {
+                        if (operatorResult[k] == '+' || operatorResult[k] == '-' || operatorResult[k] == '*' || operatorResult[k] == '/' || operatorResult[k] == '%' || operatorResult[k] == '=') {
+                            operator = operatorResult[k]
+                        }
+                    }
                 }
-                if(operator == "="){
+                
+                //연산자가 = 일 경우 반복문 탈출
+                if (operator == '=') {
                     println("총 계산 값 $tempNum")
                     break
                 }
-                newNum = temp[1].toInt()
+                newNum = tempBr[1].toInt()
 
                 tempNum = operatorFunction(false, tempNum, operator, newNum, tempNum)
             }
 
             i++
+        } catch (e: NumberFormatException) {
+            
+            //NumberFormatException 에 대한 Exception 처리
+            println("숫자가 잘못 입력 되었습니다")
         }
-    }catch (e: NumberFormatException){
-        println("숫자가 잘못 입력 되었습니다")
     }
+
 }
 
-fun operatorFunction(isFirst: Boolean, num1:Int, operator:String, num2:Int, tempNumMain:Int):Int {
+
+//계산을 처리 하는 함수
+fun operatorFunction(isFirst: Boolean, num1:Int, operator:Char, num2:Int, tempNumMain:Int):Int {
     var tempNum = tempNumMain
     if(isFirst){
         when (operator) {
-            "+" -> {
+            '+' -> {
                 tempNum += AddOperation().result(num1, num2)
                 println(tempNum)
             }
 
-            "-" -> {
+            '-' -> {
                 tempNum += SubstractOperation().result(num1, num2)
                 println(tempNum)
             }
 
-            "*" -> {
+            '*' -> {
                 tempNum += MultiplyOperation().result(num1, num2)
                 println(tempNum)
             }
 
-            "/" -> {
+            '/' -> {
                 tempNum += DivideOperation().result(num1, num2)
                 println(tempNum)
             }
 
-            "%" -> {
+            '%' -> {
                 tempNum += RemainOperation().result(num1, num2)
                 println(tempNum)
             }
@@ -89,27 +133,27 @@ fun operatorFunction(isFirst: Boolean, num1:Int, operator:String, num2:Int, temp
         return tempNum
     }else {
         when (operator) {
-            "+" -> {
+            '+' -> {
                 tempNum = AddOperation().result(num1, num2)
                 println(tempNum)
             }
 
-            "-" -> {
+            '-' -> {
                 tempNum = SubstractOperation().result(num1, num2)
                 println(tempNum)
             }
 
-            "*" -> {
+            '*' -> {
                 tempNum = MultiplyOperation().result(num1, num2)
                 println(tempNum)
             }
 
-            "/" -> {
+            '/' -> {
                 tempNum = DivideOperation().result(num1, num2)
                 println(tempNum)
             }
 
-            "%" -> {
+            '%' -> {
                 tempNum = RemainOperation().result(num1, num2)
                 println(tempNum)
             }
